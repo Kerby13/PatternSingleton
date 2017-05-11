f9bd06b7e91c515c5a8e08ddb365c7c06de67a61
Nikita Lukin <nikitadecember@mail.ru>
2017-05-11 14:37:37 +0300

    Add files via upload

---
+++ Single/Single/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singletop singl = new Singletop();
+            singl.Launch("Windows 10");
+            Console.WriteLine(singl.OS.Name);
+
+            singl.Launch(Console.ReadLine());
+            Console.WriteLine(singl.OS.Name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Single/Single/Singletop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single
+{
+    class Singletop
+    {
+        public OS OS { get; set; }
+        public void Launch(string osName)
+        {
+            OS = OS.getInstance(osName);
+        }
+    }   
+    class OS
+    {
+        private static OS instance;
+
+        public string Name { get; private set; }
+
+        private static object syncRoot = new Object();
+
+        protected OS(string name)
+        {
+            this.Name = name;
+        }
+
+        public static OS getInstance(string name)
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                        instance = new OS(name);
+                }
+            }
+            return instance;
+        }
+    }
+}
